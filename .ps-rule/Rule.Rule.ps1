0c73bd47d160ca444a8a6baa5fd6000a58e18c66
amco3 <aura.castellanos@globant.com>
2023-08-02 14:50:37 -0500

    test scr validation

--- .ps-rule/Rule.Rule.ps1
+++ .ps-rule/Rule.Rule.ps1
@@ -2,4 +2,8 @@
 # Licensed under the MIT License.
 
 # Synopsis: Use short rule names.
-Assert-PSRule  -InputPath 'br:' 
+Rule 'Rule.Name' -Type 'PSRule.Rules.Rule' {
+    Recommend 'Rule name should be less than 35 characters to prevent being truncated.'
+    Reason 'The rule name is too long.'
+    $Assert.PSRule($InputPath, 'br:') 
+}
